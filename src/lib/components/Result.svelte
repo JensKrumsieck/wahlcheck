5dabd68238342a1767f20c1fd4da038af09d7e14
Jens Krumsieck <j.krumsieck@outlook.de>
2026-08-12 22:31:45 +0200

    plot

--- src/lib/components/Result.svelte
+++ src/lib/components/Result.svelte
@@ -1,14 +1,38 @@
 <script lang="ts">
   import { computeScore, type UserAnswers } from "$lib/types";
+  import { AxisX, AxisY, BarX, Plot } from "svelteplot";
+
   interface Props {
     score: UserAnswers;
   }
 
   let { score }: Props = $props();
 
-  let derived = $derived(computeScore(score));
+  let raw = $derived(computeScore(score));
+  let values = $derived(
+    raw.map((i) => ({
+      name: i.name,
+      value: i.totalScore / i.maxScore,
+    })),
+  );
+
+  const scheme = {
+    CDU: "#55598e",
+    SPD: "#d23a33",
+    GRÜNE: "#3ca951",
+    FDP: "#efb118",
+    Linke: "#bf1d97",
+  };
 </script>
 
-{#each derived as party}
-  {party.name}: {(party.totalScore / party.maxScore) * 100} %
-{/each}
+<Plot x={{ type: "band" }} y={{ percent: true }} color={{ scheme }}>
+  <AxisX title="" />
+  <AxisY title="" />
+  <BarX
+    data={values}
+    y="name"
+    x="value"
+    fill="party"
+    sort={{ channel: "value", order: "descending" }}
+  />
+</Plot>
